9a13a526448d91638bb29c83d1d57520f7f9355f
Javier Romero <rjavier@vmware.com>
2021-05-04 06:42:41 -0500

    Add .editorconfig
    Signed-off-by: Javier Romero <rjavier@vmware.com>

--- src/tests/save-image-to-workspace_test.sh
+++ src/tests/save-image-to-workspace_test.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
 testParamImageFileRequired() {
-    local results="$(PARAM_IMAGE_NAME='name' main)" code="$?"
-    assertEquals 1 $code
-    
-    echo -e "RESULTS:\n${results}"
-    assertContains "$results" "param image-file is required!"
+  local results="$(PARAM_IMAGE_NAME='name' main)" code="$?"
+  assertEquals 1 $code
+
+  echo -e "RESULTS:\n${results}"
+  assertContains "$results" "param image-file is required!"
 }
 
 testParamImageNameRequired() {
-    local results="$(PARAM_IMAGE_FILE='file' main)" code="$?"
-    assertEquals 1 $code
-    
-    echo -e "RESULTS:\n${results}"
-    assertContains "$results" "param image-name is required!"
+  local results="$(PARAM_IMAGE_FILE='file' main)" code="$?"
+  assertEquals 1 $code
+
+  echo -e "RESULTS:\n${results}"
+  assertContains "$results" "param image-name is required!"
 }
 
 testImageFileIsCreated() {
-    cat << EOF > ${SHUNIT_TMPDIR}/Dockerfile
+  cat <<EOF >${SHUNIT_TMPDIR}/Dockerfile
 FROM busybox AS build-env
 RUN touch /empty
 
@@ -25,24 +25,24 @@ FROM scratch
 COPY --from=build-env /empty /.emptyfile
 EOF
 
-    local image_name="test$RANDOM"
-    echo "> Creating docker image..."
-    docker build -t ${image_name} ${SHUNIT_TMPDIR}
+  local image_name="test$RANDOM"
+  echo "> Creating docker image..."
+  docker build -t ${image_name} ${SHUNIT_TMPDIR}
 
-    local image_dir="${SHUNIT_TMPDIR}/images"
-    local results="$(IMAGE_DIR=$image_dir PARAM_IMAGE_NAME=$image_name PARAM_IMAGE_FILE='image.img' main)" code="$?"
-    assertEquals 0 $code
-    echo -e "RESULTS:\n${results}"
+  local image_dir="${SHUNIT_TMPDIR}/images"
+  local results="$(IMAGE_DIR=$image_dir PARAM_IMAGE_NAME=$image_name PARAM_IMAGE_FILE='image.img' main)" code="$?"
+  assertEquals 0 $code
+  echo -e "RESULTS:\n${results}"
 
-    ls -al $image_dir/image.img
+  ls -al $image_dir/image.img
 
-    assertContains "$results" "$image_dir"
-    assertContains "$results" "$image_name"
+  assertContains "$results" "$image_dir"
+  assertContains "$results" "$image_name"
 }
 
 oneTimeSetUp() {
-    echo "> Loading script under test..."
-    source './../scripts/save-image-to-workspace.sh'
+  echo "> Loading script under test..."
+  source './../scripts/save-image-to-workspace.sh'
 }
 
 # Load shUnit2
